e94be7a330a6455168b994f101f73e781e04378d
huntj0n <hunt.jon19@gmail.com>
2024-01-02 20:54:12 -0700

    opposites attract. solutions.

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function lovefunc(flower1, flower2) {
     return false;
   }
 }
+
+// function lovefunc(flower1, flower2){
+//     return flower1 % 2 !== flower2 % 2;
+//   }
+
+// function lovefunc(flower1, flower2){
+//   return (flower1 + flower2) % 2 === 1
+// }
+
+// function lovefunc(flower1, flower2){
+//     return (flower1 + flower2) % 2 == 1;
+//   }
