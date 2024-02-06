808f8fc89ce4b14885bf19883cb7bcd0dddc5c48
huntj0n <hunt.jon19@gmail.com>
2024-02-05 23:03:02 -0700

    remove first and last letter (8kyu)

--- index.js
+++ index.js
@@ -132,3 +132,14 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return i * 2;
 }
+
+/////////////
+// Remove first and last letter
+// {8kyu}
+////////////
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+function removeChar(str) {
+  //You got this!
+  return str.slice(1, -1);
+}
