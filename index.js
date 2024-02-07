e2f9dbf8bddc2e45a027f3a4698a952f756cb70a
huntj0n <hunt.jon19@gmail.com>
2024-02-06 22:30:23 -0700

    calculate bmi 8kyu

--- index.js
+++ index.js
@@ -143,3 +143,17 @@ function removeChar(str) {
   //You got this!
   return str.slice(1, -1);
 }
+
+/////////////
+// Calculate BMI
+// {8kyu}
+////////////
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
