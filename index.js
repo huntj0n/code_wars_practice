bf1dcaa5a657b27f8909334d56760c066c5d57bd
huntj0n <hunt.jon19@gmail.com>
2024-02-10 22:05:02 -0700

    Replace With Alphabet Position

--- index.js
+++ index.js
@@ -170,3 +170,35 @@ function bmi(weight, height) {
 // const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
 //                                     bmi <= 25 ? "Normal" :
 //                                     bmi <= 30 ? "Overweight" : "Obese";
+
+/////////////
+// Replace With Alphabet Position
+// {6kyu}
+////////////
+// Instructions
+// Output
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+function alphabetPosition(text) {
+  let chari,
+    arr = [],
+    alphabet = "abcdefghijklmnopqrstuvwxyz",
+    i;
+
+  for (let i = 0; i < text.length; i++) {
+    chari = text[i].toLowerCase();
+    if (alphabet.indexOf(chari) !== -1) {
+      arr.push(alphabet.indexOf(chari) + 1);
+    }
+  }
+  return arr.join(" ");
+}
