723c9f9129d28d1d1e01e1f10e12785198b43f1d
huntj0n <hunt.jon19@gmail.com>
2024-02-12 19:34:09 -0700

    tribonacci sequence

--- index.js
+++ index.js
@@ -234,3 +234,11 @@ function alphabetPosition(text) {
 // Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
 
 // Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+function tribonacci(signature, n) {
+  let trib = signature;
+  for (i = 3; i < n; i++) {
+    trib.push(trib[i - 1] + trib[i - 2] + trib[i - 3]);
+  }
+  return trib.slice(0, n);
+}
