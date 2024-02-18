9711a14457356388b774e61ab9621632b87f6d69
huntj0n <hunt.jon19@gmail.com>
2024-02-18 11:41:29 -0700

    calculate average (8kyu)

--- index.js
+++ index.js
@@ -353,3 +353,22 @@ function duplicateEncode(word) {
     })
     .join("");
 }
+
+/////////////
+// Calculate Average
+// {8 kyu}
+////////////
+//Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+function findAverage(array) {
+  // your code here
+  if (array.length === 0) return 0;
+  const average = array.reduce((total, item) => total + item) / array.length;
+  return average;
+}
+
+const find_average = (array) => {
+  return array.length === 0
+    ? 0
+    : array.reduce((acc, ind) => acc + ind, 0) / array.length;
+};
