7f13426acba26669f2968d0627da6e85923fd8fd
huntj0n <hunt.jon19@gmail.com>
2024-02-21 03:52:36 -0700

    grasshopper-gradebook (8kyu)

--- index.js
+++ index.js
@@ -461,3 +461,46 @@ function openOrSenior1(data) {
 
 const openOrSenior = (data) =>
   data.map(([age, handicap]) => (age > 54 && handicap > 7 ? "Senior" : "Open"));
+
+/////////////
+// Grasshopper - Grade Book
+// {8 kyu}
+////////////
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+function getGrade(s1, s2, s3) {
+  // Code here
+  const avg = (s1 + s2 + s3) / 3;
+
+  if (avg >= 90 && avg <= 100) {
+    return "A";
+  } else if (avg >= 80 && avg < 90) {
+    return "B";
+  } else if (avg >= 70 && avg < 80) {
+    return "C";
+  } else if (avg >= 60 && avg < 70) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+function getGrade2(s1, s2, s3) {
+  avg = (s1 + s2 + s3) / 3;
+  if (avg < 60) return "F";
+  else if (avg < 70) return "D";
+  else if (avg < 80) return "C";
+  else if (avg < 90) return "B";
+  else return "A";
+}
+
+function getGrade3(s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3;
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
+}
