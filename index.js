898f4acdf68fa8729fe600258a71a891fb0613c3
huntj0n <hunt.jon19@gmail.com>
2024-02-24 09:45:03 -0700

    Counting sheep... (8kyu)

--- index.js
+++ index.js
@@ -676,3 +676,32 @@ var isSquareLONG = function (n) {
 };
 //Math.sqrt(n) is a lot faster. basically if theres a remainder it cant be a square root.
 const isSquare = (n) => Math.sqrt(n) % 1 == 0;
+
+/////////////
+// Counting Sheep...
+// {7 kyu}
+////////////
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+function countSheepsFORLOOP(sheep) {
+  let result = 0;
+  for (let i = 0; i < sheep.length; i++) {
+    if (sheep[i]) result++;
+    else continue;
+  }
+  return result;
+}
+
+const countSheepsBOOLEAN = (sheep) => sheep.filter(Boolean).length;
+const countSheeps = (sheep) => sheep.filter((s) => s).length;
