7947a0313874917be317f590492a7ed8efd43af7
huntj0n <hunt.jon19@gmail.com>
2024-02-26 13:26:37 -0700

    your order, please (6kyu)

--- index.js
+++ index.js
@@ -729,3 +729,33 @@ function countBy2(x, n) {
 }
 
 const countBy3 = (x, n) => [...Array(n)].map((_, idx) => ++idx * x);
+
+/////////////
+// Your order, please
+// {6 kyu}
+/////////////
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  // ...
+  return words
+    .split(" ")
+    .sort((first, second) => {
+      return first.match(/\d/)[0] - second.match(/\d/)[0];
+    })
+    .join(" ");
+}
+
+var reg = /\d/;
+function order1(words) {
+  return words.split(" ").sort(comparator).join(" ");
+}
+function comparator1(word, nextWord) {
+  return +word.match(reg) - +nextWord.match(reg);
+}
