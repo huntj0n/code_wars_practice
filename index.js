55bf0e7e71c8b99c4e50816b731c7b3a09a233fe
huntj0n <hunt.jon19@gmail.com>
2024-02-26 23:57:51 -0700

    count positives/sum negatives (8kyu)

--- index.js
+++ index.js
@@ -759,3 +759,47 @@ function order1(words) {
 function comparator1(word, nextWord) {
   return +word.match(reg) - +nextWord.match(reg);
 }
+
+/////////////
+// Count of positives/sum of negatives
+// {8 kyu}
+/////////////
+/*
+Given an array of integers.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+function countPositivesSumNegativesMINE(input) {
+  // your code here
+  let count = 0;
+  let sum = 0;
+  input.map((x) => {
+    if (x === 0) sum = sum;
+    if (x === 0) count = count;
+    else x > 0 ? count++ : (sum += x);
+  });
+  return [count, sum];
+}
+
+function countPositivesSumNegatives1(input) {
+  let result = [0, 0];
+  if (input === null || input.length === 0) return [];
+
+  for (let i of input) {
+    if (i > 0) result[0]++;
+    else result[1] += i;
+  }
+
+  return result;
+}
+
+function countPositivesSumNegatives2(input) {
+  return input && input.length
+    ? [
+        input.filter((p) => p > 0).length,
+        input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
+      ]
+    : [];
+}
