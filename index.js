fbe118c77d26ea2c438ddd8bdffd488218a3d862
huntj0n <hunt.jon19@gmail.com>
2024-02-27 22:26:56 -0700

    Rock paper scissors! (8kyu)

--- index.js
+++ index.js
@@ -832,3 +832,44 @@ function areYouPlayingBanjo2(name) {
     " banjo"
   );
 }
+
+/////////////
+// Rock, Paper, Scissors
+// {8 kyu}
+/////////////
+/*
+Create a function which answers the question "Are you playing banjo?".
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+*/
+
+const rps = (p1, p2) => {
+  const one = "Player 1 won!";
+  const two = "Player 2 won!";
+  const draw = "Draw!";
+
+  if (p1 === "scissors" && p2 === "paper") return one;
+  if (p1 === "scissors" && p2 === "rock") return two;
+
+  if (p1 === "paper" && p2 === "rock") return one;
+  if (p1 === "paper" && p2 === "scissors") return two;
+
+  if (p1 === "rock" && p2 === "scissors") return one;
+  if (p1 === "rock" && p2 === "paper") return two;
+  else return draw;
+};
+
+const rps2 = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  let rules = { rock: "scissors", paper: "rock", scissors: "paper" };
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  } else {
+    return "Player 2 won!";
+  }
+};
