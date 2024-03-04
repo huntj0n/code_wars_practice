4fab2b05411d851b36fc0a589b0977384057e4d6
huntj0n <hunt.jon19@gmail.com>
2024-03-04 09:34:38 -0700

    parse nice int from char problem (8kyu)

--- index.js
+++ index.js
@@ -1119,3 +1119,74 @@ const removeExclamationMarks = (s) => s.replace(/\!/g, "");
 function removeExclamationMarks2(s) {
   return s.replaceAll("!", "");
 }
+
+/////////////
+// Mexican Wave
+// {6 kyu}
+/////////////
+/*
+The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator returns to the usual seated position.
+
+The result is a wave of standing spectators that travels through the crowd, even though individual spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements, the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although simultaneous, counter-rotating waves have been produced. (Source Wikipedia)
+
+TASK:
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+
+1.  The input string will always be lower case but maybe empty.
+
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+
+ wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+ */
+function wave(string) {
+  let result = [],
+    i;
+
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === " ") continue;
+    result.push(
+      Array.from(string, (c, j) => (i === j ? c.toUpperCase() : c)).join("")
+    );
+  }
+  return result;
+}
+
+function wave2(str) {
+  let result = [];
+
+  str.split("").forEach((char, index) => {
+    if (/[a-z]/.test(char)) {
+      result.push(
+        str.slice(0, index) + char.toUpperCase() + str.slice(index + 1)
+      );
+    }
+  });
+
+  return result;
+}
+
+function wave3(str) {
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let copy = str.split("");
+    if (copy[i] !== " ") {
+      copy[i] = copy[i].toUpperCase();
+      newArr.push(copy.join(""));
+    }
+  }
+  return newArr;
+}
+
+/////////////
+// Parse nice int from char problem
+// {8 kyu}
+/////////////
+/*
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+*/
+const getAge = (inputString) => parseInt(inputString);
