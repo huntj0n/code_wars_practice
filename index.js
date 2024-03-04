718bba9494c774fa74eb42e7797f3e14d7c9de44
huntj0n <hunt.jon19@gmail.com>
2024-03-04 09:46:16 -0700

    beginner series #2 clock (8kyu)

--- index.js
+++ index.js
@@ -1190,3 +1190,28 @@ Write a program that returns the girl's age (0-9) as an integer.
 Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 */
 const getAge = (inputString) => parseInt(inputString);
+
+/////////////
+// Beginner Series #2 Clock
+// {8 kyu}
+/////////////
+/*
+Clock shows h hours, m minutes and s seconds after midnight.
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+EX:
+  h = 0
+  m = 1
+  s = 1
+
+  result = 61000
+
+Input constraints:
+  0 <= h <= 23
+  0 <= m <= 59
+  0 <= s <= 59
+  */
+
+function past(h, m, s) {
+  //#Happy Coding! ^_^
+}
