eff60b87b50307bbd1c47dc638a9983f67e4a79b
huntj0n <hunt.jon19@gmail.com>
2024-03-11 21:58:20 -0600

    Descending order (7kyu)

--- index.js
+++ index.js
@@ -1350,3 +1350,33 @@ Complete the solution so that it reverses the string passed into it.
 const solutionReverse = (str) => str.split("").reverse().join("");
 
 const solutionReverse2 = (s) => [...s].reverse().join("");
+
+/////////////
+// Descending Order
+// {7 kyu}
+/////////////
+/*
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+*/
+const descendingOrder = (n) =>
+  Number([...String(n)].sort((a, b) => b - a).join(""));
+
+function descendingOrder2(n) {
+  return parseInt(String(n).split("").sort().reverse().join(""));
+}
+
+function descendingOrder3(n) {
+  return +(n + "")
+    .split("")
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .join("");
+}
