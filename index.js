3e179ddc7d6042ca13f16c3e3d4219341f305691
huntj0n <hunt.jon19@gmail.com>
2024-07-15 22:52:36 -0600

    extract the domain name from a URL (5kyu)

--- index.js
+++ index.js
@@ -1836,3 +1836,16 @@ function smash(words) {
 }
 
 const smash2 = (words) => words.join(" ");
+
+/////////////
+// Extract the domain name from a URL
+// {5 kyu}
+/////////////
+/*
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+*/
