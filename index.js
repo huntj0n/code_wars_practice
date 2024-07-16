abc286c1acd85be5f0f7539424bbd39cc6afd732
huntj0n <hunt.jon19@gmail.com>
2024-07-16 04:18:43 -0600

    url splice

--- index.js
+++ index.js
@@ -1849,3 +1849,20 @@ Write a function that when given a URL as a string, parses out just the domain n
 * url = "https://www.cnet.com"                -> domain name = cnet"
 
 */
+function domainName(url) {
+  url = url.replace("http://", "");
+  url = url.replace("https://", "");
+  url = url.replace("www.", "");
+
+  return url.split(".")[0];
+}
+function domainName2(url) {
+  return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+function domainName3(url) {
+  return url
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "")
+    .split(".")[0];
+}
