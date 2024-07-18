a39e9d51ec8a33c5b7a3b3d8cf85b35fcc34b290
huntj0n <hunt.jon19@gmail.com>
2024-07-18 01:41:51 -0600

    terminnal game move function (8kyu)

--- index.js
+++ index.js
@@ -1883,8 +1883,23 @@ const reverseWords = (str) =>
     .split(" ")
     .map((el) => [...el].reverse().join(""))
     .join(" ");
+
 function reverseWords2(str) {
   // Go for it
   //split words into seperate arrays
   return str.split("").reverse().join("").split(" ").reverse().join(" ");
 }
+
+/////////////
+// Grasshopper - Terminal game move function
+// {8 kyu}
+/////////////
+/*
+Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Example:
+move(3, 6) should equal 15
+*/
