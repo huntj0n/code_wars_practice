4cc26160a66b8898cfb8e0ab92bcfa10d9e721b4
huntj0n <hunt.jon19@gmail.com>
2024-10-20 20:57:24 -0600

    reverse list order (8kyu)

--- index.js
+++ index.js
@@ -2067,3 +2067,121 @@ function arrayDiff2(a, b) {
 function array_diff3(a, b) {
   return a.filter((e) => !b.includes(e));
 }
+
+/////////////
+// Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+// {6 kyu}
+/////////////
+/*
+The number 
+89
+89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number: 
+89
+=
+8
+1
++
+9
+2
+89=8 
+1
+ +9 
+2
+ 
+
+The next number in having this property is 
+135
+135:
+
+See this property again: 
+135
+=
+1
+1
++
+3
+2
++
+5
+3
+135=1 
+1
+ +3 
+2
+ +5 
+3
+ 
+
+Task
+We need a function to collect these numbers, that may receive two integers 
+a
+a, 
+b
+b that defines the range 
+[
+a
+,
+b
+]
+[a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+Examples
+Let's see some cases (input -> output):
+
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+If there are no numbers of this kind in the range 
+[
+a
+,
+b
+]
+[a,b] the function should output an empty list.
+
+90, 100 --> []
+Enjoy it!!
+*/
+const isEurekaNumber = (num) => {
+  const sum = String(num)
+    .split("")
+    .reduce((acc, digit, index) => {
+      return acc + Math.pow(Number(digit), index + 1);
+    }, 0);
+
+  return sum === num;
+};
+
+const sumDigPow = (a, b) => {
+  const eurekaNumbers = [];
+
+  for (let i = a; i <= b; i++) {
+    if (isEurekaNumber(i)) eurekaNumbers.push(i);
+  }
+
+  return eurekaNumbers;
+};
+
+function sumDigPow2(a, b) {
+  var arr = [];
+  for (var i = a; i <= b; i++) {
+    var sum = 0;
+    for (var j = 0; j <= String(i).length; j++) {
+      sum += Math.pow(parseInt(String(i)[j]), j + 1);
+      if (sum == i) arr.push(i);
+    }
+  }
+  return arr;
+}
+
+/////////////
+// Reverse List Order
+// {8 kyu}
+/////////////
+/*
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+*/
+reverseList = (list) => list.reverse();
