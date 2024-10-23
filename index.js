701357ba466e231ba1d4fe4aa7356b38e66fe04c
huntj0n <hunt.jon19@gmail.com>
2024-10-22 21:04:29 -0600

    Binary Addition (7kyu)

--- index.js
+++ index.js
@@ -2185,3 +2185,19 @@ Examples (Input -> Output)
 * [9, 2, 0, 7]  -> [7, 0, 2, 9]
 */
 reverseList = (list) => list.reverse();
+
+/////////////
+// Binary Addition
+// {7 kyu}
+/////////////
+/*
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+*/
+const addBinary = (a, b) => (a + b).toString(2);
