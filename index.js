6c24e31ef116e4e801592efd0d517e31c42db78a
huntj0n <hunt.jon19@gmail.com>
2024-11-07 18:26:15 -0700

    Students Final Grade (8kyu)

--- index.js
+++ index.js
@@ -2287,3 +2287,72 @@ Example output:
 Hello, Mr. Spock
 */
 const sayHello = (name) => `Hello, ${name}`;
+
+/////////////
+// Find the next perfect square!
+// {7 kyu}
+/////////////
+/*
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples ( Input --> Output )
+121 --> 144
+625 --> 676
+114 --> -1  #  because 114 is not a perfect square
+*/
+let findNextSquare = (sq) =>
+  Math.sqrt(sq) % 1 ? -1 : (Math.sqrt(sq) + 1) ** 2;
+
+/////////////
+// Student's Final Grade
+// {8 kyu}
+/////////////
+/*
+Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+Examples(Inputs-->Output):
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0
+*/
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+
+const finalGradeImproved = (exam, projects) => {
+  return exam > 90 || projects > 10
+    ? 100
+    : exam > 75 && projects >= 5
+    ? 90
+    : exam > 50 && projects >= 2
+    ? 75
+    : 0;
+};
