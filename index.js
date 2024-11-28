906e48f6881ec59aa3e8552931e2f576f3d139b7
huntj0n <hunt.jon19@gmail.com>
2024-11-27 18:51:59 -0700

    convert a number to a string (8kyu)

--- index.js
+++ index.js
@@ -2455,3 +2455,23 @@ const capitals4 = function (word) {
     return v === v.toUpperCase() ? memo.concat(i) : memo;
   }, []);
 };
+
+/////////////
+// Convert a Number to a String!
+// {7 kyu}
+/////////////
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+*/
+function numberToString(num) {
+  return "" + num;
+}
+
+const numberToString2 = (num) => `${num}`;
