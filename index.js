bf93487ab6a37086773f3664b15cfeaef82f67ec
huntj0n <hunt.jon19@gmail.com>
2024-11-28 12:37:18 -0700

    Transportation on vacation (8kyu)

--- index.js
+++ index.js
@@ -2545,3 +2545,36 @@ const getChar = (c) => {
 };
 
 const getChar2 = String.fromCharCode;
+
+/////////////
+// Transportation on vacation
+// {8 kyu}
+/////////////
+/*
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).
+*/
+function rentalCarCost(d) {
+  let price;
+
+  if (d < 3) {
+    price = d * 40;
+  } else if (d < 7) {
+    price = d * 40 - 20;
+  } else {
+    price = d * 40 - 50;
+  }
+
+  return price;
+}
+
+function rentalCarCost2(d) {
+  return d * 40 - (d >= 7 ? 50 : d >= 3 ? 20 : 0);
+}
+
+const rentalCarCost3 = (d) => d * 40 - (d > 6 ? 50 : d > 2 ? 20 : 0);
