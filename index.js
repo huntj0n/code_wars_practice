eb19cbcb0753b8fafc343f38a19e3e6aea23e1e5
huntj0n <hunt.jon19@gmail.com>
2024-12-02 19:38:55 -0700

    summing a numbers digits (7kyu)

--- index.js
+++ index.js
@@ -2603,3 +2603,24 @@ function smallEnough3(a, limit) {
   }
   return true;
 }
+
+/////////////
+// Summing a number's digits
+// {7 kyu}
+/////////////
+/*
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+*/
+function sumDigits(num) {
+  return Math.abs(num)
+    .toString()
+    .split("")
+    .reduce((total, next) => total + Number(next), 0);
+}
